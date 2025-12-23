aedd78ff299413ece0abf3a7c433e1a630632b43
Preto T.I <epilif.cire@gmail.com>
2025-12-23 07:19:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-self.addEventListener("install", e=>{
+self.addEventListener("install",e=>{
   e.waitUntil(
     caches.open("treino-v1").then(c=>c.addAll([
       "./",
       "index.html",
       "style.css",
-      "app.js"
+      "app.js",
+      "manifest.json"
     ]))
   )
 })
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch",e=>{
   e.respondWith(
     caches.match(e.request).then(r=>r||fetch(e.request))
   )
